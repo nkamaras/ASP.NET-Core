b427735b971f2fbb9ce3c1b1c4a69f200f3de2f9
nkamaras <nikos.7kamaras@gmail.com>
2023-02-24 20:50:13 +0200

    Add files via upload

---
+++ Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp.Shared.Models
+{
+    public class Game
+    {
+        public int GameId { get; set; }
+        public string GameDescription { get; set; }
+        public int TeamAId { get; set; }
+        public int TeamBId { get; set; }
+        public string GameHour { get; set; }
+        public int attendees { get; set; }
+        public int StadiumId { get; set; }
+        public virtual Stadium Stadium { get; set; }
+    }
+}
---
+++ Models/Stadium.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoApp.Shared.Models
+{
+    public class Stadium
+    {
+        public int StadiumId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public List<Game> Games { get; set; }
+    }
+}
